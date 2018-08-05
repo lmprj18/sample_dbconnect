415ccf3f5ec6c2ebce092e060cc2d0386c88cf65
Kenneth Kim <kenneth@Kennethui-MacBook-Pro.local>
2018-08-05 16:13:22 +0900

    collection names attached

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://lmprj18:KDTLj9RnKzrWSgsXEkTEzh4J0zPSfPP5fJhi7gipA11jD7KBLzLqpTU14uwOr3MiC0G4NG2vqXItsHSsciFSiA%3D%3D@lmprj18.documents.azure.com:10255/?ssl=true';
 
+//Collection 이름
+//기훈:lm_kenneth
+//성한:lm_zino
+//정민:lm_dave
+//태준:lm_bobby
+//*위 콜렉션이름은 이미 생성되어있으니 'families'대신 자신의 콜렉션에다가 CRUD하면됨
+var CollectionName = "families";
+
 var insertDocument = function(db, callback) {
-db.collection('families').insertOne( {
+db.collection(CollectionName).insertOne( {
         "id": "AndersenFamily",
         "lastName": "Andersen",
         "parents": [
